10b053311ce7d4edd8369f15a3b85d2c499926be
FerchoGD <ferchogir@gmail.com>
2019-07-18 15:33:38 -0500

    ArchivoResult

--- Cuda/RetoCuda.cpp
+++ Cuda/RetoCuda.cpp
@@ -4,6 +4,8 @@
 #include <time.h>
 #include "../common/common.h"
 #include <cuda_runtime.h>
+#include <fstream.h>
+#include <omp.h>
 
 using namespace std;
 
@@ -70,12 +72,61 @@ void simulationonhost(int *A,int size,int iter){
     
 }
 
-__global__ void simulationongpu(int *v,const int size, int iteraciones){
+__global__ void simulationongpu(int *A,const int size, int iter){
     int i= threadIdx.x;
-    if(i<iteraciones) 
+    if(i<iter) {
+        int iteraciones=0;
+    
+        int *new_traffic = new int[size];
+        bool change_ult=false;
+
+        for (int i=0; i<size-1;i++){
+            if(A[i]==1){
+                if(A[i+1]==0){
+                    new_traffic[i]=0;
+                    new_traffic[i+1]=1;
+                    cout<<"Hola ";
+                    i++;
+                    if(i>=size-1)change_ult=true;                
+                }
+                else{
+                new_traffic[i]=A[i];
+                cout<<" Da igual ";
+                }
+            }
+            else{
+                new_traffic[i]=A[i];
+                cout<<" Da igual ";
+                }
+            
+            
+        }
+        //Comprobando último elemento
+
+        if(A[0]==0 && A[size-1]==1){
+            new_traffic[0]=1;
+            new_traffic[size-1]=0;
+            cout<<" Hola ult ";
+            change_ult=true;
+        }
+        if(!change_ult){
+            new_traffic[size-1]=A[size-1];
+            cout<<"Da igual ult ";
+        }        
+        cout<<"Iteración #"<<iteraciones<<" : "<<endl;
+        for(int j=0;j<size;j++){
+            A[j]=new_traffic[j];
+            cout<<A[j]<<" ";
+        }
+        cout<<endl;
+        iteraciones++;
+        delete new_traffic;
+    
+    }
 }
 
 int main(){
+    srand(time(NULL));
     int tam=10;
     int *vehiculos = new int[tam];
     int simulations=5;
@@ -85,7 +136,36 @@ int main(){
         cout<<vehiculos[ind]<<" ";
     }
     cout<<endl;
+    ofstream archivo("resultados.txt");
+
+    //Configurando CUDA
+
+    
+
+
+
+
+
+
+
+
+    //Simulación en Secuencial
     cout<<"Simulating Traffic: "<<endl;
+    double start = omp_get_wtime();
     simulationonhost(vehiculos,tam,simulations);
+    double finish = omp_get_wtime( );
+    double time = finish - start;
+    fs<<time<<"-"<<tam<<"-"<<simulations<<"\n";
+
+    //Simulación en Paralelo
+    start= omp_get_wtime();
+    simulationongpu(vehiculos,tam,simulations);
+    finish = omp_get_wtime();
+    time = finish - start;
+    fs<<"Cuda: "<<tiempo<<"-"<<tam<<"-"<<simulations<<"\n";
+
+
+
+    archivo.close();
     delete vehiculos;
 }
